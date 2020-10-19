3b04b7d006b7b61703a61efaeae54c864ddefd08
sohan90568 <43694042+sohan90568@users.noreply.github.com>
2020-10-19 23:26:50 +0600

    Intial Setup

---
+++ MyEcommerceShop/Myshop.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Myshop.Web.Startup))]
+namespace Myshop.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
