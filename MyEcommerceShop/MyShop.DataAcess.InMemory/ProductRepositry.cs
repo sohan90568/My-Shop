7537c6f61382f3f7aba379ae575af2e51da868c6
sohan90568 <43694042+sohan90568@users.noreply.github.com>
2020-10-22 23:22:22 +0600

    Product in Memory and Repositry

---
+++ MyEcommerceShop/MyShop.DataAcess.InMemory/ProductRepositry.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Caching;
+using MyShop.Core.Models;
+
+namespace MyShop.DataAcess.InMemory
+{
+    public class ProductRepositry
+
+    {
+        ObjectCache cache = MemoryCache.Default;
+        List<Products> products;
+
+        public ProductRepositry()
+        {
+            products = cache["products"] as List<Products>;
+            if(products==null)
+            {
+                products = new List<Products>();
+            }
+
+            
+        }
+
+        public void commit()
+        {
+            cache["products"] = products;
+        }
+
+
+    }
+}
